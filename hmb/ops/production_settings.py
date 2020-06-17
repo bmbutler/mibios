915fa8d988ec440a53a08bd4a8fc6d1ad5624dbd
Robert <heinro@umich.edu>
2020-06-17 21:55:47 +0000

    settings: fix path to static root

--- hmb/ops/production_settings.py
+++ hmb/ops/production_settings.py
@@ -6,5 +6,5 @@ from .settings import *
 DEBUG = False
 # TODO: secretly get SECRET_KEY
 INSTALLED_APPS = [i for i in INSTALLED_APPS if i not in DEV_ONLY_APPS]
-STATIC_ROOT = '/use/share/hmb/static'
+STATIC_ROOT = '/usr/share/hmb/static'
 DATABASES['default']['NAME'] = '/var/lib/hmb/db.sqlite3'
