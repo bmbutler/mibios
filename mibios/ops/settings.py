c3bc915d7b2c3433c2bfc664d1710d21d2447b6d
Robert <heinro@umich.edu>
2020-06-19 20:41:51 +0000

    settings: allow localhost
    this permits testing via ./manage runserver with debug=False

--- mibios/ops/settings.py
+++ mibios/ops/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'f++e=zx!e*0ri!gdak!o@adl@e$#q+^v@=ffjh(^_k7xp@k8=k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
