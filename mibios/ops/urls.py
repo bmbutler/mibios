faa392b51f6a161c653b508be98ad03fb1839f53
Robert <heinro@umich.edu>
2020-06-19 02:42:28 +0000

    remove url prefix, not needed

--- mibios/ops/urls.py
+++ mibios/ops/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mibios/', include('mibios.urls')),
+    path('', include('mibios.urls')),
 ]
