93cfbeae278f277a91dfcd62da5253e8923308e0
Robert <heinro@umich.edu>
2020-06-24 09:48:55 -0400

    derive local settings from production

--- mibios/ops/local_settings.py
+++ mibios/ops/local_settings.py
@@ -2,14 +2,9 @@
 Settings for production environment for use in manually installed setups, copy
 this file to the web applications working directory as ./settings.py
 """
-from mibios.ops.settings import *
+from .production_settings import *
 
 DEBUG = False
-# TODO: secretly get SECRET_KEY
-INSTALLED_APPS = [i for i in INSTALLED_APPS if i not in DEV_ONLY_APPS]
 STATIC_ROOT = './static'
 DATABASES['default']['NAME'] = './db.sqlite3'
 ALLOWED_HOSTS = ['localhost']
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.RemoteUserBackend',
-]
--- mibios/ops/production_settings.py
+++ mibios/ops/production_settings.py
@@ -3,12 +3,14 @@ Settings for production environment for system-wide installations
 """
 from .settings import *
 
-# TODO: secretly get SECRET_KEY
 INSTALLED_APPS = [i for i in INSTALLED_APPS if i not in DEV_ONLY_APPS]
 STATIC_ROOT = '/usr/share/mibios/static'
 DATABASES['default']['NAME'] = '/var/lib/mibios/db.sqlite3'
 # allowed hosts still need to be configured further:
 ALLOWED_HOSTS = ['localhost']
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.RemoteUserBackend',
+]
 
 # Security related:
 DEBUG = False
