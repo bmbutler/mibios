9c90e247790ed1e0d8e02f6cd2040e3d5de0a9cf
Robert <heinro@umich.edu>
2020-06-24 09:10:47 -0400

    Security-harden settings

--- mibios/ops/production_settings.py
+++ mibios/ops/production_settings.py
@@ -3,10 +3,19 @@ Settings for production environment for system-wide installations
 """
 from .settings import *
 
-DEBUG = False
 # TODO: secretly get SECRET_KEY
 INSTALLED_APPS = [i for i in INSTALLED_APPS if i not in DEV_ONLY_APPS]
 STATIC_ROOT = '/usr/share/mibios/static'
 DATABASES['default']['NAME'] = '/var/lib/mibios/db.sqlite3'
 # allowed hosts still need to be configured further:
 ALLOWED_HOSTS = ['localhost']
+
+# Security related:
+DEBUG = False
+# SECURE_HSTS_SECONDS = ??  (TODO)
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
