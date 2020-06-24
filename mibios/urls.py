5ae9507c5911c8e3f7c3278db23d0adbd539161b
Robert <heinro@umich.edu>
2020-06-24 17:20:29 -0400

    Comment out test url - can be re-enable as needed in dev

--- mibios/urls.py
+++ mibios/urls.py
@@ -8,7 +8,7 @@ from .views import ExportView, ImportView, TableView, TestView
 urlpatterns = [
     path('admin/', admin_site.urls),
     path('', TableView.as_view(), name='top'),
-    path('test/', TestView.as_view(), name='test'),
+    # path('test/', TestView.as_view(), name='test'),
     path('<str:dataset>/', TableView.as_view(), name='queryset_index'),
     path('<str:dataset>/import/', ImportView.as_view(), name='import'),
     path('<str:dataset>/export/<str:format>/', ExportView.as_view(),
