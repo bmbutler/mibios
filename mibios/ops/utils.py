efeb37c18e58169431436f00041863fdeb74c9a2
Robert <heinro@umich.edu>
2020-06-30 10:42:32 -0400

    Allow switching users via settings, when using remote users

--- mibios/ops/utils.py
+++ mibios/ops/utils.py
@@ -1,6 +1,20 @@
+from django.conf import settings
 from django.contrib.auth import backends
 
 
 class RemoteUserBackend(backends.RemoteUserBackend):
     create_unknown_user = False
 
+    def clean_username(self, username):
+        """
+        Allow server admin to pretend to be a different user
+        """
+        try:
+            real, assumed = settings.ASSUME_USER_IDENTITY
+        except Exception:
+            pass
+        else:
+            if username == real:
+                username = assumed
+
+        return username
