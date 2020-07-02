4e0c394274bc5cedf61df579dbc337164ddeca8f
Robert <heinro@umich.edu>
2020-07-02 12:16:52 -0400

    auth: add user injection middle ware to help in dev
    The setting is now called ASSUME_IDENTITY, can be used in development to
    set the desired user, with which we get the anonymous user.  In
    development the first componend of ASSUME_IDENTITY is ignores.  In
    production we can replace one real authenticated user with a chosen one.

--- mibios/ops/utils.py
+++ mibios/ops/utils.py
@@ -1,5 +1,10 @@
+import logging
+
 from django.conf import settings
 from django.contrib.auth import backends
+
+
+log = logging.getLogger(__name__)
 
 
 class RemoteUserBackend(backends.RemoteUserBackend):
@@ -10,11 +15,46 @@ class RemoteUserBackend(backends.RemoteUserBackend):
         Allow server admin to pretend to be a different user
         """
         try:
-            real, assumed = settings.ASSUME_USER_IDENTITY
+            real, assumed = settings.ASSUME_IDENTITY
         except Exception:
             pass
         else:
             if username == real:
+                log.debug(
+                    'Setting REMOTE_USER from {} to {}'.format(real, assumed)
+                )
                 username = assumed
 
         return username
+
+
+class RemoteUserInjection:
+    """
+    Middleware to manipulate the REMOTE_USER variable on incoming requests
+
+    Do not use this in production.  For development, we most commonly want to
+    an authorized user to be logged in when testing views etc. and thus prevent
+    the authentication framework to give us a login with the anonymous user,
+    who shouldn't be able to access those views.  This middleware should run at
+    the earliest point.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        """
+        Set REMOTE_USER if configured
+        """
+        try:
+            _, assumed = settings.ASSUME_IDENTITY
+        except Exception:
+            pass
+        else:
+            log.debug('Setting REMOTE_USER to {}'.format(assumed))
+            request.META['REMOTE_USER'] = assumed
+            print('MIDDLEWARE BORK:', type(request.META))
+            print('MIDDLEWARE BORK:', request.META['REMOTE_USER'])
+
+        return self.get_response(request)
