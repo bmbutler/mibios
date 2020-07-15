26f514d5579bb8b19afcc9247d158c9f5ba8efd2
Robert <heinro@umich.edu>
2020-07-15 14:57:24 -0400

    urls: simplify import statement

--- mibios/urls.py
+++ mibios/urls.py
@@ -1,19 +1,19 @@
 # from django.apps import apps
 from django.urls import path
 
+from . import views
 from .admin import site as admin_site
-from .views import ExportView, HistoryView, ImportView, TableView, TestView
 
 
 urlpatterns = [
     # take over admin history view with our own
-    path('admin/mibios/<str:dataset>/<int:pk>/history/', HistoryView.as_view(),
-         name='history'),
+    path('admin/mibios/<str:dataset>/<int:pk>/history/',
+         views.HistoryView.as_view(), name='history'),
     path('admin/', admin_site.urls),
     path('', TableView.as_view(), name='top'),
     # path('test/', TestView.as_view(), name='test'),
-    path('<str:dataset>/', TableView.as_view(), name='queryset_index'),
-    path('<str:dataset>/import/', ImportView.as_view(), name='import'),
-    path('<str:dataset>/export/<str:format>/', ExportView.as_view(),
+    path('<str:dataset>/', views.TableView.as_view(), name='queryset_index'),
+    path('<str:dataset>/import/', views.ImportView.as_view(), name='import'),
+    path('<str:dataset>/export/<str:format>/', views.ExportView.as_view(),
          name='export'),
 ]
