514e171105fed4cc02c3e38638d4de61ed31d2f3
Robert <heinro@umich.edu>
2020-09-17 20:29:41 +0000

    urls: modularize urlpatterns build up a bit

--- mibios/urls.py
+++ mibios/urls.py
@@ -10,35 +10,45 @@ rest_router = routers.DefaultRouter()
 for i in get_registry().get_models():
     rest_router.register(i._meta.model_name, i.get_rest_api_viewset_class())
 
+archive_urls = [
+    # URLs starting with archive/
+    path('', views.SnapshotListView.as_view(), name='snapshot_list'),
+    path('<str:name>/', views.SnapshotView.as_view(), name='snapshot'),
+    path('<str:name>/<str:app>/<str:table>',
+         views.SnapshotTableView.as_view(), name='snapshot_table'),
+    path('<str:name>/<str:app>/<str:table>/export/<str:format>/',
+         views.ExportSnapshotTableView.as_view(),
+         name='export_snapshot_table'),
+]
+
+data_name_urls = [
+    # URLs starting with the data name
+    path('', views.TableView.as_view(), name='queryset_index'),
+    path('mean/<str:avg_by>/', views.AverageView.as_view(),
+         name='average'),
+    path('import/', views.ImportView.as_view(), name='import'),
+    path('export-form/', views.ExportFormView.as_view(),
+         name='export_form'),
+    path('export/', views.ExportView.as_view(),
+         name='export'),
+    path('mean/<str:avg_by>/export-form/',
+         views.AverageExportFormView.as_view(), name='average_export_form'),
+    path('mean/<str:avg_by>/export/',
+         views.AverageExportView.as_view(), name='average_export'),
+    path('history/deleted/', views.DeletedHistoryView.as_view(),
+         name='deleted_history'),
+    path('<str:natural>/history/', views.HistoryView.as_view(),
+         name='record_history'),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.FrontPageView.as_view(), name='top'),
     # path('test/', TestView.as_view(), name='test'),
-    path('archive/', views.SnapshotListView.as_view(), name='snapshot_list'),
-    path('archive/<str:name>/', views.SnapshotView.as_view(), name='snapshot'),
-    path('archive/<str:name>/<str:app>/<str:table>',
-         views.SnapshotTableView.as_view(), name='snapshot_table'),
-    path('archive/<str:name>/<str:app>/<str:table>/export/<str:format>/',
-         views.ExportSnapshotTableView.as_view(),
-         name='export_snapshot_table'),
+    path('archive/', include(archive_urls)),
     path('api/', include(rest_router.urls)),
     path(settings.MEDIA_URL.lstrip('/') + 'imported/<int:year>/<str:name>',
          views.ImportFileDownloadView.as_view(), name='import_file_download'),
     # fixed string paths go above this comment
-    path('<str:data_name>/', views.TableView.as_view(), name='queryset_index'),
-    path('<str:data_name>/mean/<str:avg_by>/', views.AverageView.as_view(),
-         name='average'),
-    path('<str:data_name>/import/', views.ImportView.as_view(), name='import'),
-    path('<str:data_name>/export-form/', views.ExportFormView.as_view(),
-         name='export_form'),
-    path('<str:data_name>/export/', views.ExportView.as_view(),
-         name='export'),
-    path('<str:data_name>/mean/<str:avg_by>/export-form/',
-         views.AverageExportFormView.as_view(), name='average_export_form'),
-    path('<str:data_name>/mean/<str:avg_by>/export/',
-         views.AverageExportView.as_view(), name='average_export'),
-    path('<str:data_name>/history/deleted/', views.DeletedHistoryView.as_view(),
-         name='deleted_history'),
-    path('<str:data_name>/<str:natural>/history/', views.HistoryView.as_view(),
-         name='record_history'),
+    path('<str:data_name>/', include(data_name_urls)),
 ]
