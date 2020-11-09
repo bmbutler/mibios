524f29978920c2e114e110ed7e1f7d60328e2815
Robert <heinro@umich.edu>
2020-11-09 17:45:25 +0000

    views/html: Offer shared export on abundance table with mixed projects
    The export form will ask from which project the data will come from

--- mibios_seq/views.py
+++ mibios_seq/views.py
@@ -11,21 +11,12 @@ from mibios_seq import models
 from .forms import ExportSharedForm
 
 
+log = getLogger(__name__)
+
 
 class AbundancePlugin(TableViewPlugin):
     model_class = models.Abundance
     template_name = 'mibios_seq/abundance_plugin.html'
-
-    def get_context_data(self, **ctx):
-        view = ctx['view']
-
-        # only offer exporting shared data if single project:
-        stats = view.get_queryset().get_field_stats('project', natural=True)
-        if 'uniform' in stats:
-            ctx['uniform_project'] = list(stats['uniform'].keys())[0]
-        else:
-            ctx['uniform_project'] = None
-        return ctx
 
 
 class ExportSharedFormMixin:
