ed6e5379fc71f90f2350ccddbcf0bda909acd895
Robert <heinro@umich.edu>
2021-10-04 16:58:17 -0400

    cache: fix possible crash when using dummy cache backend
    So, the dummy backend has no _cache attribute. It seems that, when
    this bug was discovered, the clear-on-save function was triggered
    while the session was written to the DB.  This not the moment we want to
    erase the cache.  Needs some investigation.

--- mibios/signals.py
+++ mibios/signals.py
@@ -15,6 +15,10 @@ def clear_cache_on_save(sender, **kwargs):
     Empty the cache when something gets saved
     """
     c = caches['default']
+    if not hasattr(c, '_cache'):
+        # dummy cache
+        return
+
     if c._cache:
         log.debug(f'cache: clearing all {len(c._cache.keys())} entries')
         c.clear()
