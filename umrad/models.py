52ceaea728f13e5eca8f60db0ad814b97cde916b
Robert <heinro@umich.edu>
2021-11-18 20:55:56 -0500

    Bootstrapping umrad app, fourth stage -- adding taxonomy

--- umrad/models.py
+++ umrad/models.py
@@ -873,6 +873,173 @@ class Sample(Model):
                 / 'storage' / 'bin_stats.analyze.tsv')
 
 
+class SimpleTaxonomyName(Model):
+    history = None
+
+    RANKS = (
+        (0, 'root'),
+        (1, 'domain'),
+        (2, 'phylum'),
+        (3, 'class', 'klass'),
+        (4, 'order'),
+        (5, 'family'),
+        (6, 'genus'),
+        (7, 'species'),
+        (8, 'strain'),
+    )
+    RANK_CHOICE = ((i[0], i[1]) for i in RANKS)
+
+    rank = models.PositiveSmallIntegerField(choices=RANK_CHOICE)
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        unique_together = (('rank', 'name'),)
+
+    @classmethod
+    @atomic
+    def load(cls, path=None):
+        if path is None:
+            path = cls.get_taxonomy_path()
+
+        data = []
+        with path.open() as f:
+            pp = ProgressPrinter('{progress} taxa found')
+            count = 0
+            log.info(f'reading taxonomy: {path}')
+            for line in f:
+                data.append(line.strip().split('\t'))
+                count += 1
+                pp.update(count)
+
+            pp.finish()
+
+        pp = ProgressPrinter('{progress} tax names processed')
+        count = 0
+        rankids = [i[0] for i in SimpleTaxonomyName.RANKS[1:]]
+        names = dict()
+        for row in data:
+            for rid, name in zip_longest(rankids, row[1:]):
+                if rid is None:
+                    raise RuntimeError(f'unexpectedly low ranks: {row}')
+                if name is None:
+                    # no strain etc
+                    continue
+                key = (rid, name)
+                count += 1
+                pp.update(count)
+                if key not in names:
+                    names[key] = SimpleTaxonomyName(rank=rid, name=name)
+
+        pp.finish()
+
+        log.info(f'Storing {len(names)} unique tax names to DB...')
+        SimpleTaxonomyName.objects.bulk_create(names.values())
+
+        return data
+
+    @classmethod
+    def get_taxonomy_path(cls):
+        return (settings.GLAMR_DATA_ROOT / 'NCRNA' / 'RNA_CENTRAL'
+                / 'TAXONOMY_DB_2021.txt')
+
+
+class SimpleTaxonomy(Model):
+    history = None
+
+    taxid = models.PositiveIntegerField(unique=True)
+    domain = models.ForeignKey(
+        SimpleTaxonomyName, **fk_opt,
+        related_name='tax_dom_rel',
+    )
+    phylum = models.ForeignKey(
+        SimpleTaxonomyName, **fk_opt,
+        related_name='tax_phy_rel',
+    )
+    klass = models.ForeignKey(
+        SimpleTaxonomyName, **fk_opt,
+        related_name='tax_cls_rel',
+    )
+    order = models.ForeignKey(
+        SimpleTaxonomyName, **fk_opt,
+        related_name='tax_ord_rel',
+    )
+    family = models.ForeignKey(
+        SimpleTaxonomyName, **fk_opt,
+        related_name='tax_fam_rel',
+    )
+    genus = models.ForeignKey(
+        SimpleTaxonomyName, **fk_opt,
+        related_name='tax_gen_rel',
+    )
+    species = models.ForeignKey(
+        SimpleTaxonomyName, **fk_opt,
+        related_name='tax_sp_rel',
+    )
+    strain = models.ForeignKey(
+        SimpleTaxonomyName, **fk_opt,
+        related_name='tax_str_rel',
+    )
+
+    def __str__(self):
+        return f'{self.taxid} {"|".join(self.get_lineage())}'
+
+    def get_lineage(self):
+        lineage = []
+        for i in SimpleTaxonomyName.RANKS[1:]:
+            name = getattr(self, i[-1], None)
+            if name is None:
+                break
+            else:
+                lineage.append(name.name)
+        return lineage
+
+    @classmethod
+    @atomic
+    def load(cls, path=None):
+        data = SimpleTaxonomyName.load(path)
+
+        # reloading names to get the ids, depends on order the fields are
+        # declared
+        names = {
+            (rank, name): pk for pk, rank, name
+            in SimpleTaxonomyName.objects.values_list().iterator()
+        }
+
+        pp = ProgressPrinter('{progress} taxa processed')
+        count = 0
+        objs = []
+        # ranks: get pairs of rank id and rank field attribute name
+        ranks = [(i[0], i[-1]) for i in SimpleTaxonomyName.RANKS[1:]]
+        for row in data:
+            kwargs = dict(taxid=row[0])
+            for (rid, attr), name in zip_longest(ranks, row[1:]):
+                # we should always have rid, attr here since we went through
+                # data before, name may be None for missing low ranks
+                if name is not None:
+                    # assign ids directly
+                    kwargs[attr + '_id'] = names[(rid, name)]
+
+            objs.append(cls(**kwargs))
+            count += 1
+            pp.update(count)
+
+        pp.finish()
+        log.info(f'Storing {len(objs)} taxa in DB...')
+        cls.objects.bulk_create(objs)
+
+    @classmethod
+    def classified(cls, lineage):
+        """ remove unclassified tail of a lineage """
+        ranks = [i[1].upper() for i in cls.RANK_CHOICE[1:]]
+        ret = lineage[:1]  # keep first
+        for rank, name in zip(ranks, lineage[1:]):
+            if name == f'UNCLASSIFIED_{ret[-1]}_{rank}':
+                break
+            ret.append(name)
+
+        return ret
+
+
 class Taxonomy(Model):
     SOURCE_DB_CHOICES = (
         (0, 'ncbi'),
