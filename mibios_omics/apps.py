61df9f0e1a7713ce09016fd6b2e769beee5d0aa4
Robert <heinro@umich.edu>
2022-03-21 12:55:15 -0400

    Add version info to app configs so it appears in html output

--- mibios_omics/apps.py
+++ mibios_omics/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig as _AppConfig
 
 
+from mibios import __version__
+
 class AppConfig(_AppConfig):
     name = 'mibios_omics'
     verbose_name = 'omics'
+    version = __version__
--- mibios_umrad/apps.py
+++ mibios_umrad/apps.py
@@ -1,6 +1,9 @@
 import django.apps
 
+from mibios import __version__
+
 
 class AppConfig(django.apps.AppConfig):
     name = 'mibios_umrad'
     verbose_name = 'UMRAD'
+    version = __version__
