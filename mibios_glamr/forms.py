266b0394038e8d345d788d2a4cd8d4a3e2becc74
Robert <heinro@umich.edu>
2022-05-16 14:45:25 -0400

    html: render the Q filter editor item form for bootstrap 5 and in single line
      * form-wide errors are displayed in row above
      * unsure how field-related error will be displayed

--- mibios_glamr/forms.py
+++ mibios_glamr/forms.py
@@ -1,7 +1,25 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms.widgets import Select, TextInput
+# from django.utils.safestring import mark_safe
 
 from .templatetags.glamr_extras import human_lookups
+
+
+class RenderMixin:
+    """ extra (experimental) rendering methods for forms """
+    def as_bootstrap(self):
+        "Return this form rendered as HTML for bootstrap 5 'single line'."
+        html = self._html_output(
+            normal_row='%(errors)s<span class="input-group-text">%(label)s</span>%(field)s%(help_text)s<span></span>',  # noqa: E501
+            error_row='</div><div class="input-group input-group-sm mb-3 alert-danger">%s</div><div class="input-group">',  # noqa: E501
+            row_ender='</span>',  # noqa:E501 ## hack with the span thing at the normal row end to make hidden fields work
+            help_text_html='<span class="input-group-text">%s</span>',
+            errors_on_separate_row=False,
+        )
+        # FIXME: ugly hack (that doesn't work)
+        # html = mark_safe(html.replace('<label', '<label class="form-label"'))
+        return html
 
 
 class SearchForm(forms.Form):
@@ -40,11 +58,12 @@ class QBuilderForm(forms.Form):
             )
 
 
-class QLeafEditForm(QBuilderForm):
+class QLeafEditForm(RenderMixin, QBuilderForm):
     """ Form to add/edit a key/value filter item """
-    key = forms.ChoiceField()
-    lookup = forms.ChoiceField(choices=human_lookups.items())
-    value = forms.CharField()
+    key = forms.ChoiceField(widget=Select(attrs={'class': 'form-select'}))
+    lookup = forms.ChoiceField(choices=human_lookups.items(),
+                               widget=Select(attrs={'class': 'form-select'}))
+    value = forms.CharField(widget=TextInput(attrs={'class': 'form-control'}))
     add_mode = forms.BooleanField(
         required=False,  # to allow this to be False
         widget=forms.HiddenInput()
