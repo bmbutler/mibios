c8d380890af7922017438f0330a626b116376197
Robert <heinro@umich.edu>
2022-05-19 16:51:29 -0400

    Split into two databases: django stuff vs. omics/umrad/mibios data

--- mibios_omics/settings.py
+++ mibios_omics/settings.py
@@ -1,6 +1,8 @@
 """
 settings for the mibios_omics app
 """
+from os import environ
+
 from mibios.ops.settings import *  # noqa:F403
 
 # hook up the apps
@@ -13,3 +15,34 @@ OMICS_SAMPLE_GROUP_MODEL = 'mibios_omics.SampleGroup'
 
 # register logging
 LOGGING['loggers']['mibios_omics'] = LOGGING['loggers']['mibios']  # noqa:F405
+
+
+def setdb(db_dir='.', db_infix=''):
+    """
+    Call this to set DATABASES
+
+    db_dir:  Directory where to store the DBs, without trailing slash
+    db_infix: optional infix to distinguish alternative DBs
+    """
+    ops_db_file = f'ops{db_infix}.sqlite3'
+    omics_db_file = f'omics{db_infix}.sqlite3'
+    omics_db_mode = 'ro' if environ.get('MIBIOS_DB_RO') else 'rwc'
+
+    global DATABASES
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': f'file:{db_dir}/{ops_db_file}?mode=rwc',
+            'OPTIONS': {'uri': True},
+        },
+        'omics': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': f'file:{db_dir}/{omics_db_file}?mode={omics_db_mode}',
+            'OPTIONS': {'uri': True},
+        }
+    }
+
+
+setdb()
+
+DATABASE_ROUTERS = ['mibios_omics.DBRouter']
