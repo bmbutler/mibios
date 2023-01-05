4c105cde581a0089ffd84e416cf1e30e4004e133
Robert <heinro@umich.edu>
2023-01-05 16:46:20 -0500

    settings: tweaked settings in mibios.ops.settings
      * added DEFAULT_AUTO_FIELD, somehow this needs to be set (since Django
        3.2)
      * review the example settings
      * add separate example settings for GLAMR setup, this is a separate
        file since we import default settings from a different module for
        plain mibios vs. glamr

--- mibios/ops/settings.py
+++ mibios/ops/settings.py
@@ -15,9 +15,10 @@ from pathlib import Path
 
 from . import get_secret_key
 
-
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_secret_key(Path('./secret.key'))
@@ -33,8 +34,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 
-ALLOWED_HOSTS = ['localhost']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
