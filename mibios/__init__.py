be9a20d3c698e47b9c09a6bae9224669203950a1
Robert <heinro@umich.edu>
2023-01-11 11:29:23 -0500

    Populate __version__ with git describe output in development settings

--- mibios/__init__.py
+++ mibios/__init__.py
@@ -1,5 +1,21 @@
 __version__ = ''  # to be overwritten by`setup.py build`
 
+if __version__ == '':
+    # get a version for development settings, in production the __version__
+    # variable should have a hard-coded value.  For robustness all exceptions
+    # are caught and __version__ is not set in case of errors.
+    # get something like v0.9-339-g68a6ee6 via git-describe
+    cmd = 'git describe --tag --match v* --always'
+    try:
+        import subprocess
+        p = subprocess.run(
+            cmd.split(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        __version__ = p.stdout.decode().split(maxsplit=1)[0]
+    except Exception:
+        pass
 
 # constants used in TableView and forms, e.g. keyword recognized in the URL
 # querystring, declared here to avoid circular imports
